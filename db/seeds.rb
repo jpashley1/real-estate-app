bd792d2d4156e5ef8c63ab3ff84de05b8c059bce
jpashley1 <jpashley1@gmail.com>
2024-09-03 19:09:26 -0400

    added model instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Home.create(description: "Ocean Views, what else is there to say?", year_built: 1968, square_feet: 2160, bedrooms: 3, bathrooms: 3, floors: 1, availability: true, address: "1336 Pacific Coast Hwy", price: 34999999)
+
+# Home.create(description: "Right near da beach?", year_built: 1967, square_feet: 2400, bedrooms: 4, bathrooms: 3, floors: 1, availability: true, address: "1787 Pacific Coast Hwy", price: 41999999)
+
+# Home.create(description: "Paradise awaits at this mid-century gem.", year_built: 1962, square_feet: 2600, bedrooms: 2, bathrooms: 2, floors: 2, availability: true, address: "1558 Pacific Coast Hwy", price: 2899999)
